09c07402e6338c059a6bd177a18582debc247871
jharris49 <22jharris@wakatipu.school.nz>
2025-07-20 22:30:59 +1200

    Functional charts and database data output
    New Features:
    - Data can now be fetched and displayed directly from the database
    - Users are able to select what date they want to view their screen time data on
    - Daily data is now displayed in a pie chart using percentages so users what percentage of their day they are using on their devices
    
    Other:
    - Minor UI tweaks

--- DIT Major Project/DIT Major Project/ContentView.swift
+++ DIT Major Project/DIT Major Project/ContentView.swift
@@ -21,7 +21,7 @@ struct HomeView: View {
                     Label("Input Data", systemImage: "rectangle.and.pencil.and.ellipsis")
                 }
                 .tag(0)
-            DataView(time: $time)
+            DataView()
                 .tabItem {
                     Label("View Data", systemImage: "chart.pie.fill")
                 }
@@ -83,41 +83,129 @@ struct InputView: View {
     func addData() {
         let newData = Screentime(context: viewContext)
         newData.date = selectedDate
-        newData.hours = Int64(time) ?? 0
+        newData.hours = Double(time) ?? 0
         do {
             try viewContext.save()
         } catch {
     
         }
     }
+    
 }
-  
+
+struct pChartData: Identifiable {
+    var id = UUID()
+    var category: String
+    var hours: Double
+}
 
 struct DataView: View {
-    @Binding var time: String
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Screentime.date, ascending: true)]
+    ) var allData: FetchedResults<Screentime>
+    @State private var wantedDate = Date()
+    
+    
     var body: some View {
-        NavigationView {
-            VStack {
-            let yearlyTime = (Int(time) ?? 0 ) * 365
-            if yearlyTime == 0 {
-                Text("Please enter some hours")
-            } else {
-                Text("At this rate you will spend \(yearlyTime) hours on your phone this year")
+        var todayData: [Screentime] {
+            let calendar = Calendar.current
+            let start = calendar.startOfDay(for: wantedDate)
+            let end = calendar.date(byAdding: .day, value: 1, to: start)!
+            
+            return allData.filter { entry in
+                if let date = entry.date {
+                    return date >= start && date < end
+                }
+                return false
             }
         }
-        .navigationTitle("Data")
-        .toolbar{
-            ToolbarItem(placement: .navigationBarLeading) { NavigationLink(
-                destination: AccountView()){
-                    Image(systemName: "person.crop.circle.fill")
-                }
+        
+        var screenTimeToday: [Screentime] {
+            return todayData.filter { data in
+                return data.hours > 0
             }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                NavigationLink(
-                    destination: HelpView()){
-                        Image(systemName: "questionmark")
+        }
+        
+        var screenTimeDouble: Double {
+            var total: Double = 0
+               for entry in screenTimeToday {
+                   total += entry.hours
+               }
+               return Double(total)
+        }
+        
+        var averageSleep: Double = 8
+        var averageNecessities: Double = 2.5
+        
+        var pCatagories: [pChartData] { [
+            .init(category: "Screentime", hours: screenTimeDouble),
+            .init(category: "Sleep", hours: averageSleep),
+            .init(category: "Necessities", hours: averageNecessities),
+            .init(category: "Other", hours: 24 - screenTimeDouble - averageSleep - averageNecessities)
+        ]
+        }
+        
+        return NavigationView {
+            VStack {
+                HStack{
+                    Button {
+                        wantedDate = Calendar.current.date(byAdding: .day, value: -1, to: wantedDate)!
+                        
+                    } label: {
+                        Image(systemName: "arrowshape.left.fill")
                     }
-            }
+                    DatePicker("", selection: $wantedDate, displayedComponents: [.date])
+                        .labelsHidden()
+                    Button {
+                        wantedDate = Calendar.current.date(byAdding: .day, value: 1, to: wantedDate)!
+                        
+                    } label: {
+                        Image(systemName: "arrowshape.right.fill")
+                    }
+                }
+                VStack(spacing: 1) {
+                    
+                    Chart(pCatagories) { category in
+                        SectorMark(
+                            angle: .value(Text(verbatim: category.category), category.hours), innerRadius: .ratio(0.5),
+                            angularInset: 2
+                        )
+                        
+                        .foregroundStyle(by: .value(Text(verbatim: category.category), category.category))
+                        .annotation(position: .overlay) {
+                            Text("\(String(format: "%.0f", category.hours * 100/24))% (\(String(format: "%.1f", category.hours))h)")
+                                .foregroundStyle(.white)
+                                .font(.system(size:12))
+                    }
+            
+                    }
+                    .padding(.bottom, -10)
+                }
+                .padding(35)
+                
+                    /*
+                     List(todayData) { entry in
+                         Text("Hours: \(String(format: "%.1f", entry.hours))")
+                     Text("Date: \(entry.date?.formatted() ?? "Unknown")")
+                     }
+                     */
+                
+                .navigationTitle("Data")
+                .toolbar{
+                    ToolbarItem(placement: .navigationBarLeading) { NavigationLink(
+                        destination: AccountView()){
+                            Image(systemName: "person.crop.circle.fill")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(
+                            destination: HelpView()){
+                                Image(systemName: "questionmark")
+                            }
+                    }
+                }
             }
         }
     }
@@ -146,5 +234,16 @@ struct HelpView: View {
 }
 
 #Preview {
-    HomeView()
+    let context = PersistenceController.preview.container.viewContext
+    
+    let sample = Screentime(context: context)
+    let sample2 = Screentime(context: context)
+    let components = DateComponents(year: 2025, month: 7, day: 23)
+    sample.date = Date()
+    sample.hours = Double(5)
+    sample2.date = Calendar.current.date(from: components)!
+    sample2.hours = Double(10)
+       
+    return HomeView()
+           .environment(\.managedObjectContext, context)
 }
