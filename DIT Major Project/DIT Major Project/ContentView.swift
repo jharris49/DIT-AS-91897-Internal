7a57e65197f4a06c1d474e58c453fcfc344dd109
jharris49 <22jharris@wakatipu.school.nz>
2025-07-28 09:19:59 +1200

    Functional sleeptime and some error messages. Updated code structure.
    New Features:
    - Users can now select and save how much time they slept for on a day
    - Most invalid options for inputted sleeptime data are stopped and handled
    
    Edited features:
    - Updated database structure in order to be able to save sleeptime
    
    Other:
    - Minor UI and code structure changes

--- DIT Major Project/DIT Major Project/ContentView.swift
+++ DIT Major Project/DIT Major Project/ContentView.swift
@@ -33,12 +33,46 @@ struct HomeView: View {
 
 struct InputView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    
     @Binding var selectedTab: Int
     @Binding var time: String
     @State private var showTabs = true
     @State private var message = ""
     @State private var selectedDate = Date()
+    @State var showAlert = false
+    @State var hoursAsleep = 8
+    @State var minutesAsleep = 0
+    
+    private var sleepPicker: some View {
+        HStack {
+            Picker("", selection: $hoursAsleep){
+                ForEach(0..<25) {
+                    Text("\($0) hours")
+                }
+            }
+            if hoursAsleep < 24 {
+                Picker("",selection: $minutesAsleep){
+                    ForEach(0..<60){
+                        Text("\($0) minutes")
+                    }
+                }
+            } else {
+                Picker("",selection: $minutesAsleep){
+                        Text("0 minutes")
+                    }
+                }
+            }
+        .pickerStyle(.wheel)
+            .clipped()
+            .frame(height: 80)
+    }
+    
+    var tooManyHours: Bool {
+        if (Double(time) ?? 0) > 24 || (Double(time) ?? 0) < 0 {
+            return true
+        } else {
+            return false
+        }
+    }
     
     var body: some View {
         NavigationStack{
@@ -54,43 +88,63 @@ struct InputView: View {
                         DatePicker("Date",
                                    selection: $selectedDate,
                                    displayedComponents: [.date])
-                        Button {
+                    }
+                    Section ("Other") {
+                        HStack{
+                        LabeledContent(content: { sleepPicker }, label: { Text("Sleep") })
+                        }
+                    }
+                    Button {
+                        if !tooManyHours {
                             selectedTab = 1
                             addData()
-                        } label: {
-                            Text("Confirm")
-                        }.frame(maxWidth: .infinity)
-                    }
+                        } else {
+                            showAlert = true
+                        }
+                    } label: {
+                        Text("Confirm")
+                    }.frame(maxWidth: .infinity)
                 }
                 .navigationTitle("Input Data")
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading) { NavigationLink(
-                        destination: AccountView()){
-                            Image(systemName: "person.crop.circle.fill")
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(
-                            destination: HelpView()){
-                                Image(systemName: "questionmark")
-                            }
-                    }
-                }
+                .toolbar{ mainToolbar()}
+            }
+            .alert("Invalid Hours", isPresented: $showAlert) {
+                Button("Ok", role: .cancel) {}
+            } message: {
+                Text("Please enter a valid value")
             }
         }
     }
     func addData() {
-        let newData = Screentime(context: viewContext)
-        newData.date = selectedDate
-        newData.hours = Double(time) ?? 0
-        do {
-            try viewContext.save()
-        } catch {
-    
+        let totalTimeAsleep = Double(hoursAsleep) + Double(minutesAsleep)/60
+            let newData = DailyData(context: viewContext)
+            newData.date = selectedDate
+            newData.screentime = Double(time) ?? 0
+            newData.sleeptime = totalTimeAsleep
+            do {
+                try viewContext.save()
+            } catch {
+                
+            }
+        }
+}
+
+
+struct mainToolbar: ToolbarContent {
+    var body: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) { NavigationLink(
+            destination: AccountView()){
+                Image(systemName: "person.crop.circle.fill")
+            }
+        }
+        ToolbarItem(placement: .navigationBarTrailing) {
+            NavigationLink(
+                destination: HelpView()){
+                    Image(systemName: "questionmark")
+                }
         }
     }
-    
 }
 
 struct pChartData: Identifiable {
@@ -103,13 +157,13 @@ struct DataView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Screentime.date, ascending: true)]
-    ) var allData: FetchedResults<Screentime>
+        sortDescriptors: [NSSortDescriptor(keyPath: \DailyData.date, ascending: true)]
+    ) var allData: FetchedResults<DailyData>
     @State private var wantedDate = Date()
     
     
     var body: some View {
-        var todayData: [Screentime] {
+        var todayData: [DailyData] {
             let calendar = Calendar.current
             let start = calendar.startOfDay(for: wantedDate)
             let end = calendar.date(byAdding: .day, value: 1, to: start)!
@@ -122,16 +176,16 @@ struct DataView: View {
             }
         }
         
-        var screenTimeToday: [Screentime] {
+        var screenTimeToday: [DailyData] {
             return todayData.filter { data in
-                return data.hours > 0
+                return data.screentime > 0
             }
         }
         
         var screenTimeDouble: Double {
             var total: Double = 0
                for entry in screenTimeToday {
-                   total += entry.hours
+                   total += entry.screentime
                }
                return Double(total)
         }
@@ -193,19 +247,7 @@ struct DataView: View {
                      */
                 
                 .navigationTitle("Data")
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading) { NavigationLink(
-                        destination: AccountView()){
-                            Image(systemName: "person.crop.circle.fill")
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(
-                            destination: HelpView()){
-                                Image(systemName: "questionmark")
-                            }
-                    }
-                }
+                .toolbar{mainToolbar()}
             }
         }
     }
@@ -236,13 +278,13 @@ struct HelpView: View {
 #Preview {
     let context = PersistenceController.preview.container.viewContext
     
-    let sample = Screentime(context: context)
-    let sample2 = Screentime(context: context)
+    let sample = DailyData(context: context)
+    let sample2 = DailyData(context: context)
     let components = DateComponents(year: 2025, month: 7, day: 23)
     sample.date = Date()
-    sample.hours = Double(5)
+    sample.screentime = Double(5)
     sample2.date = Calendar.current.date(from: components)!
-    sample2.hours = Double(10)
+    sample2.screentime = Double(10)
        
     return HomeView()
            .environment(\.managedObjectContext, context)
